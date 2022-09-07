f8b0d1a20b1c08f1b2a6f48c8cac5633df90bc99
dangel0n <ura.vlasov.ru@gmail.com>
2022-09-07 20:40:43 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,21 @@ $(function () {
   $('.testimonials__slider').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-right.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
 
     appendArrows: $('.testimonials__item'),
     appendDots: $('.testimonials__item'),
-    autoplay: true,
-    autoplaySpeed: 3000,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
     responsive: [{
-      breakpoints: 992,
+      breakpoint: 768,
       settings: {
-        arrows: false,
+        dots: false,
+        appendArrows: $('.testimonials__slider'),
       }
     }]
-    
   })
+
   $('.menu__burger').on('click', function () {
     $('.menu-mob').toggleClass('menu-mob__active');
   });
@@ -24,78 +25,38 @@ $(function () {
     $('.menu-mob').toggleClass('menu-mob__active');
   });
 
-  // const mql = window.matchMedia(mediaQueryString);
-
- 
-
-  // if (window.matchMedia("(min-width: 992px)").matches) {
-  //   $('.restaurant__content').slick('unslick');
-  //   sliderIsLive = false;
-  // } else {
-  //   $('.restaurant__content').slick({
-  //     dots: true,
-  //     arrows: false,
-  //     autoplay: true,
-  //     autoplaySpeed: 3000
-  //   });
-  //   sliderIsLive = true;
-  // };
-
   if (window.matchMedia("(min-width: 992px)").matches) {
-
+    $('.restaurant__content').slick('unslick');
+    sliderIsLive = false;
   } else {
     $('.restaurant__content').slick({
       dots: true,
       arrows: false,
       autoplay: true,
       autoplaySpeed: 3000
+    });
+    sliderIsLive = true;
+
+    $('.testimonials__slider').slick({
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          dots: false,
+          appendArrows: $('.testimonials__slider'),
+        }
+      }]
+
     })
-  }
+  };
+  // if (window.matchMedia("(min-width: 768px)").matches) {
+
+  // } else {
+  //   $('.testimonials__slider').slick({
+  //     dots: false,
+  //     appendArrows: $('.testimonials__slider'),
+  //   })
+  // };
 
   var mixer = mixitup('.popular__catalog');
 })
 
-// const mql = window.matchMedia(mediaQueryString);
-
-// function name(params) {
-//   if (window.matchMedia("(min-width: 992px)").matches) {
-
-//   } else {
-//     $('.restaurant__content').slick({
-//       dots: true,
-//       arrows: false,
-//       autoplay: true,
-//       autoplaySpeed: 3000
-//     })
-//   }
-// }
-
-// if (window.matchMedia("(min-width: 992px)").matches) {
-
-// } else {
-//   $('.restaurant__content').slick({
-//     dots: true,
-//     arrows: false,
-//     autoplay: true,
-//     autoplaySpeed: 3000
-//   })
-// }
-
-
-
-
-
-
-  // $('.testimonials__slider').slick({
-  //   dots: true,
-  //   prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
-  //   nextArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-right.svg" alt=""></button>',
-  //   autoplay: true,
-  //   autoplaySpeed: 3000,
-  //   responsive: [{
-  //     breakpoints: 992,
-  //     settings: {
-  //       arrows: false,
-  //     }
-  //   }]
-  // });
