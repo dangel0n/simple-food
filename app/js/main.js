5efc915c65c75d1b517125c468f5ea3208d1c653
dangel0n <ura.vlasov.ru@gmail.com>
2022-09-29 19:40:55 +0300

    product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,44 @@
+
+
 $(function () {
+
+  $('.interest__slider').slick({
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
+    appendArrows: $('.interest__title'),
+  })
+
+  $('.product-top__slider').slick({
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
+  })
+
+  $('.product-tabs__top-item').on('click', function(e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item ').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+  $(".star").rateYo({
+    starWidth:"17px",
+    normalFill: "#C1C1C1",
+    ratedFill: "#FFB800",
+    readOnly: true,
+  });
+
+  $(".review__star").rateYo({
+    starWidth: "17px",
+    normalFill: "#C1C1C1",
+    ratedFill: "#FFB800",
+  });
 
   $('.pagination__link').on('click', function () {
     $('.pagination__link').removeClass('pagination__link--active');
@@ -28,7 +68,9 @@ $(function () {
 
   });
 
+
   $('.select-style').styler();
+  $('.product-top__input').styler();
 
   $('.testimonials__slider').slick({
     dots: true,
@@ -90,6 +132,7 @@ $(function () {
       autoplaySpeed: 3000,
     })
   }
+
 
   var mixer = mixitup('.popular__catalog');
 })
